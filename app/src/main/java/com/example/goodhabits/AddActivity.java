ec630b236f6b5ad659c4a95639ee78561d2b966b
Farhan Akib Rahman <winter@Farhans-MacBook-Pro.local>
2021-02-19 02:44:21 -0600

    Made the Add_Activity look better, added Toast Messages, implemented RadioButtons into the Add_Activity and linked the Home Screen to Add_Activity

--- app/src/main/java/com/example/goodhabits/AddActivity.java
+++ app/src/main/java/com/example/goodhabits/AddActivity.java
@@ -7,28 +7,33 @@ import androidx.fragment.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
     //Fields
     private String name;
-    private boolean type;
+    private RadioGroup Habit_type;
+    private RadioButton type_button;
     private String msg;
-    private int hour;
-    private int minute;
+    private int hour=-1;//Initialized to -1
+    private int minute=-1;//Initialized to -1
+    private boolean ToastFired=false;//Used to check if any toast messages were given
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         //Contexts of the form that need to be stored
         EditText Habit_Name;
-        Switch Habit_Type;
         EditText User_Msg;
         Button time_Btn;
         Button Submit_Btn;
@@ -41,18 +46,25 @@ public class AddActivity extends AppCompatActivity implements TimePickerDialog.O
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        //Initializing the fields
-        //When the name of the Habit is Written
+        //Get the name of the Habit when it is Written
         Habit_Name=findViewById(R.id.habit_name);
 
-        //When the type of Habit is selected
-        Habit_Type=findViewById(R.id.habit_switch);
+        //Tapping a RadioButton to determine type of Habit
+        Habit_type=findViewById(R.id.radiogroup);
+        Habit_type.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                //Assign the new selected button to type_button
+                type_button=findViewById(id);
+            }
+        });
+
 
         //When the User message is written
         User_Msg=findViewById(R.id.habit_msg);
 
         //Tapping the Time Picker Button to select a time for the activity regarding the habit
-        time_Btn=(Button)findViewById(R.id.time_picker);
+        time_Btn=findViewById(R.id.time_picker);
         time_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,33 +73,49 @@ public class AddActivity extends AppCompatActivity implements TimePickerDialog.O
             }
         });
 
-        //Tapping the "Submit" Button on the create form
+        //Tapping the "Submit" Button on the addHabit form
         Submit_Btn=findViewById(R.id.submit_button);
         Submit_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //This boolean value is used to check if any toast messages are given
+                ToastFired=false;
                 //Get the name of the habit
                 name=Habit_Name.getText().toString();
-                assert name.length()>0 : "You have to give a name to your Habit";
-                //Get the type of the habit
-                type=Habit_Type.isChecked();
-                assert type==true || type==false : "Type of habit is not selected?";
+
+                //Get the HabitType selected (If the user does not click on the RadioButtons)
+                int radioId=Habit_type.getCheckedRadioButtonId();
+                type_button=findViewById(radioId);
+
                 //Get the User message
                 msg=User_Msg.getText().toString();
-                //The User message can be empty if the user decides to
-                if(msg.length()==0)
-                {
-                    msg="";
+
+                //Check for toast message
+                checkToaster();
+
+                //If any Toast message is NOT displayed
+                if(ToastFired==false) {
+                    //Create a Habit
+                    Habit newHabit=createHabit(name,type_button,msg,hour,minute);
+                    //Add the Habit to the list of Habits
+                    //addHabit(newHabit);
+                    //Go to the main screen
+                    startActivity(new Intent(AddActivity.this, MainActivity.class));
                 }
-                //Create a Habit
-                Habit newHabit=createHabit(name,type,msg,hour,minute);
-                //Add the Habit to the list of Habits
-                //addHabit(newHabit);
-                //Go to Home Screen
-                startActivity(new Intent(AddActivity.this,MainActivity.class));
             }
         });
     }
+
+    //This Function finds the RadioButton that is selected by the user
+    public void selectedButton(View v)
+    {
+        //Get the ID of the selected button
+        int radioId= Habit_type.getCheckedRadioButtonId();
+        //Assign it to our RadioButton Variable
+        type_button=findViewById(radioId);
+
+    }
+
     //This Function displays the time selected and stores the time selected by the user
     @Override
     public void onTimeSet(TimePicker timePicker, int hourOfDay, int minuteOfHour) {
@@ -96,11 +124,8 @@ public class AddActivity extends AppCompatActivity implements TimePickerDialog.O
         textView.setText("Hour: "+hourOfDay+" Minute: "+minuteOfHour);
         //Store the time selected by the user
         hour=hourOfDay;
-        assert hour>0 && hour<25 : "Invalid Time";
         minute=minuteOfHour;
-        assert minute>=0 && minute<60 : "Invalid Time";
     }
-
 
 
     // Delegate function that recognises the tap on back button of this Activity
@@ -113,9 +138,16 @@ public class AddActivity extends AppCompatActivity implements TimePickerDialog.O
     }
 
     //This function creates a new Habit
-    public Habit createHabit(String hName, boolean hType, String hMsg, int hTime, int mTime)
-    {
-        return new Habit(hName,hType,hMsg,hTime,mTime);
+    public Habit createHabit(String hName, RadioButton hType, String hMsg, int hTime, int mTime) {
+        //A boolean variable that is true for Good_Habit and false for Bad_Habit
+        boolean boolType;
+        if (hType.getId() == findViewById(R.id.Good_Habit).getId()) {
+            boolType = true;
+        } else
+        {
+            boolType=false;
+        }
+        return new Habit(hName,boolType,hMsg,hTime,mTime);
     }
 
     //This Function Adds a new Habit to the list of Habits
@@ -124,8 +156,27 @@ public class AddActivity extends AppCompatActivity implements TimePickerDialog.O
 //
 //    }
 
+    public void checkToaster()
+    {
+        //Check if the user selected a time
+        if(hour==-1 && minute==-1)
+        {
+            Toast.makeText(this,"You have to select a time",Toast.LENGTH_SHORT).show();
+            ToastFired=true;
+        }
+        //Check if the user selected a type of Habit
+        if(Habit_type.getCheckedRadioButtonId()==-1)
+        {
+            Toast.makeText(this,"You have select a type of Habit",Toast.LENGTH_SHORT).show();
+            ToastFired=true;
+        }
+        //Check if the user gave a Habit a name
+        if(name==null || name.length()<1)
+        {
+            Toast.makeText(this,"You have to assign a name to your Habit",Toast.LENGTH_SHORT).show();
+            ToastFired=true;
+        }
 
-
-
+    }
 }
 
--- app/src/main/java/com/example/goodhabits/Habit.java
+++ app/src/main/java/com/example/goodhabits/Habit.java
@@ -2,10 +2,10 @@ package com.example.goodhabits;
 //This class is a Data structure for "Habit"
 public class Habit {
     private String name;//Name of Habit
-    private boolean type;//Type of Habit, If true then it is a "Good Habit" else it is a "Bad Habbit"
+    private boolean type;//Type of Habit, If true then it is a "Good Habit" else it is a "Bad Habit"
     private String msg;//A message that the user writes inorder to engage in an activity
     private int hour;//Used to handle the hour of the day for the activity
-    private int minute;//Used to handle the minute of the hour for the acivity
+    private int minute;//Used to handle the minute of the hour for the activity
 
     public Habit(String Name,boolean Type,String Msg,int Hour,int Minute)
     {
@@ -20,7 +20,7 @@ public class Habit {
     {
         return this.name;
     }
-    //Returns Type of Habit, If true then it is a "Good Habit" else it is a "Bad Habbit"
+    //Returns Type of Habit, If true then it is a "Good Habit" else it is a "Bad Habit"
     public boolean HabitType()
     {
         return this.type;
