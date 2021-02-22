ba6b44993789a0ad94e53350c715a3fda14c495d
Jiaming <chenj327@myumanitoba.ca>
2021-02-21 21:44:12 -0600

    Implemented AllHabits Activity and created list view

--- app/src/main/java/com/example/goodhabits/Persistence/HabitStorage.java
+++ app/src/main/java/com/example/goodhabits/Persistence/HabitStorage.java
@@ -5,19 +5,26 @@ import com.example.goodhabits.Objects.Habit;
 import java.util.ArrayList;
 
 public class HabitStorage {
-    private static ArrayList<Habit> habitStorage;
+    private static ArrayList<Habit> habitStorage = new ArrayList<>();
 
     public HabitStorage(){
-        habitStorage = new ArrayList<>();
     }
 
-    public void addToHabitStorage(Habit habit){
+    public static void addToHabitStorage(Habit habit){
         habitStorage.add(habit);
     }
 
     public static ArrayList<Habit> getHabitStorage(){
         return habitStorage;
     }
+    public static String[] getAllName(){
+        String[] result = new String[habitStorage.size()];
+        for(int i=0; i<habitStorage.size(); i++){
+            result[i] = habitStorage.get(i).getHabitName();
+        }
+        return result;
+    }
+
 
     public static int getStorageSize(){
         return habitStorage.size();
--- app/src/main/java/com/example/goodhabits/Presentation/AllHabitsActivity.java
+++ app/src/main/java/com/example/goodhabits/Presentation/AllHabitsActivity.java
@@ -4,17 +4,89 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.goodhabits.Objects.Profile;
+import com.example.goodhabits.Persistence.HabitStorage;
+import com.example.goodhabits.Objects.Habit;
+
+
+import com.example.goodhabits.Persistence.HabitStorage;
+import com.example.goodhabits.Persistence.ProfileStorage;
 import com.example.goodhabits.R;
+
+import java.util.ArrayList;
 
 
 public class AllHabitsActivity extends AppCompatActivity {
 
+    ListView listView;
+    boolean test = true;
+   // HabitStorage storage = new HabitStorage();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_habits);
 
+        listView = (ListView)findViewById(R.id.listview);
+        //build new arraylist and add some valve
+
+
+        if(test) {
+            HabitStorage.addToHabitStorage(new Habit("gym", true, "every day", 1, 20));
+            HabitStorage.addToHabitStorage(new Habit("school", true, "monday", 1, 40));
+            HabitStorage.addToHabitStorage(new Habit("home", true, "go back", 5, 30));
+            test = false;
+        }
+        //shallow copy all habit item from storage to arrlist
+        //ArrayList<Habit> arrlist=HabitStorage.getHabitStorage();
+
+
+
+
+        //show name on table
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,HabitStorage.getAllName());
+        listView.setAdapter(arrayAdapter);
+        //show message when user click box
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+             //   Toast.makeText(AllHabitsActivity.this,HabitStorage.getAllName()+" "+HabitStorage.getAllName().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /*
+        listView = (ListView)findViewById(R.id.listview);
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        //add value to arrayList
+        arrayList.add("a");
+        arrayList.add("b");
+        arrayList.add("c");
+        arrayList.add("d");
+        arrayList.add("e");
+        arrayList.add("f");
+        arrayList.add("g");
+        arrayList.add("h");
+        arrayList.add("i");
+        arrayList.add("j");
+        arrayList.add("k");
+        arrayList.add("l");
+        //show name on table
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+        //show message when user click box
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Toast.makeText(AllHabitsActivity.this,"clicked item"+i+" "+arrayList.get(i).toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+           */
         // Title Bar text
         setTitle("All Habits");
 
